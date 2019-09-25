29ca2ece15359fa9e5c589ce391c091450d891e5
Joe Ruello <joeruello@gmail.com>
2019-09-25 10:51:32 +1000

    adds decision support

---
+++ src/core/documentation/decision.ts
@@ -0,0 +1,63 @@
+import { Element } from "../model/element";
+import { Format } from "./format";
+import { DecisionStatus } from "./decisionStatus";
+
+export interface DecisionDto {
+  id: string;
+  date: string;
+  status: DecisionStatus;
+  title: string;
+  content: string;
+  format: Format;
+  elementId?: string;
+}
+
+export class Decision {
+  id: string;
+  date: Date;
+  status: DecisionStatus;
+  element: Element | null;
+  title: string;
+  format: Format;
+  content: string;
+
+  constructor(
+    element: Element | null,
+    id: string,
+    date: Date,
+    title: string,
+    status: DecisionStatus,
+    format: Format,
+    content: string
+  ) {
+    this.element = element;
+    this.id = id;
+    this.date = date;
+    this.title = title;
+    this.status = status;
+    this.format = format;
+    this.content = content;
+  }
+
+  private _elementId?: string;
+
+  get elementId() {
+    return this.element ? this.element.id : this._elementId;
+  }
+
+  set elementId(value) {
+    this._elementId = value;
+  }
+
+  toDto(): DecisionDto {
+    return {
+      id: this.id,
+      date: this.date.toISOString(),
+      status: this.status,
+      title: this.title,
+      content: this.content,
+      format: this.format,
+      elementId: this.elementId
+    };
+  }
+}
--- src/core/documentation/documentation.ts
+++ src/core/documentation/documentation.ts
@@ -2,14 +2,20 @@ import { Model } from "../model/model";
 import { Element } from "../model/element";
 import * as assert from "assert";
 import { Section, SectionDto } from "./section";
+import { Decision, DecisionDto } from "./decision";
+import { SoftwareSystem } from "../model/softwareSystem";
+import { DecisionStatus } from "./decisionStatus";
+import { Format } from "..";
 
 interface DocumentationDto {
   sections?: SectionDto[];
+  decisions?: DecisionDto[];
 }
 
 export class Documentation {
   private model: Model;
   sections: Set<Section> = new Set();
+  decisions: Set<Decision> = new Set();
 
   constructor(model: Model) {
     this.model = model;
@@ -25,17 +31,55 @@ export class Documentation {
     assert(title, "A title must be specified.");
     assert(content, "Content must be specified.");
     assert(format, "A format must be specified.");
-    this.checkSectionIsUnique(element, title);
+    checkIsUnqiue(this.sections, "title", title, element);
 
-    this.sections.add(
-      new Section(element, title, this.sections.size + 1, format, content)
+    const section = new Section(
+      element,
+      title,
+      this.sections.size + 1,
+      format,
+      content
     );
+    this.sections.add(section);
+    return section;
+  }
+
+  addDecision(
+    element: SoftwareSystem,
+    id: string,
+    date: Date,
+    title: string,
+    status: DecisionStatus,
+    format: Format,
+    content: string
+  ) {
+    assert(id, `An id must be specifed`);
+    assert(date, "A date must be specified");
+    assert(title, "A title must be specified");
+    assert(status, "A status must be specified");
+    assert(format, "A format must be specified");
+    checkIsUnqiue(this.decisions, "id", id, element);
+
+    const decision = new Decision(
+      element,
+      id,
+      date,
+      title,
+      status,
+      format,
+      content
+    );
+    this.decisions.add(decision);
+    return decision;
   }
 
   toDto(): DocumentationDto {
     return {
       sections: Array.from(this.sections.values()).map(section =>
         section.toDto()
+      ),
+      decisions: Array.from(this.decisions.values()).map(decision =>
+        decision.toDto()
       )
     };
   }
@@ -56,31 +100,60 @@ export class Documentation {
         this.sections.add(section);
       });
     }
+
+    if (dto.decisions) {
+      dto.decisions.map(dto => {
+        const decision = new Decision(
+          null,
+          dto.id,
+          new Date(dto.date),
+          dto.title,
+          dto.status,
+          dto.format,
+          dto.content
+        );
+        if (dto.elementId) {
+          decision.elementId = dto.elementId;
+        }
+        this.decisions.add(decision);
+      });
+    }
   }
 
   hydrate() {
-    this.sections.forEach(section => {
-      if (section.elementId) {
-        section.element = this.model.getElement(section.elementId);
-      }
-    });
+    this.sections.forEach(section => this.findElementAndHydrate(section));
+    this.decisions.forEach(decision => this.findElementAndHydrate(decision));
   }
 
-  private checkSectionIsUnique(element: Element, title: string) {
-    if (element) {
-      this.sections.forEach(section => {
-        assert(
-          element.id === section.elementId && title === section.title,
-          `A section with a title of ${title}  already exists for the element named  ${element.name}`
-        );
-      });
-    } else {
-      this.sections.forEach(section => {
-        assert(
-          !section.element && title === section.title,
-          `A section with a title of ${title} already exists for this workspace.`
-        );
-      });
+  private findElementAndHydrate(section: {
+    elementId?: string;
+    element: Element | null;
+  }) {
+    if (section.elementId) {
+      section.element = this.model.getElement(section.elementId);
     }
+  }
+}
+
+function checkIsUnqiue(
+  collection: Set<any>,
+  idField: string,
+  id: string,
+  element?: Element
+) {
+  if (element) {
+    collection.forEach(item => {
+      assert(
+        element.id === item.elementId && id === item[idField],
+        `A section with an ${idField} of ${id}  already exists for the element named ${element.name}`
+      );
+    });
+  } else {
+    collection.forEach(item => {
+      assert(
+        !item.element && id === item[idField],
+        `A section with an ${idField} of ${id} already exists for this workspace.`
+      );
+    });
   }
 }
