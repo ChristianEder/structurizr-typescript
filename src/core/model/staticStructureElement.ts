4ae62781d70090e037b6b847f76d4569842add8b
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2019-12-17 23:54:41 +0100

    Enable adding relationships to Components

--- src/core/model/staticStructureElement.ts
+++ src/core/model/staticStructureElement.ts
@@ -3,10 +3,11 @@ import { SoftwareSystem } from "./softwareSystem";
 import { Relationship } from "./relationship";
 import { InteractionStyle } from "./interactionStyle";
 import { Container } from "./container";
+import {Component} from "./component";
 
 export abstract class StaticStructureElement extends Element {
 
-    public uses(destination: SoftwareSystem | Container, description: string, technology?: string, interactionStyle = InteractionStyle.Synchronous): Relationship | null {
+    public uses(destination: SoftwareSystem | Container | Component, description: string, technology?: string, interactionStyle = InteractionStyle.Synchronous): Relationship | null {
         return this.model.addRelationship(this, destination, description, technology, interactionStyle);
     }
 
