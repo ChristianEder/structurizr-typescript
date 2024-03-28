677d9b6647ceaea2dd712d5647f625fac2b5c7ee
Christian Eder <christian.johann.eder@googlemail.com>
2024-03-28 14:58:00 +0100

    Trying with delays to make api tests more stable, improve logging

--- test/api-compatibility.ts
+++ test/api-compatibility.ts
@@ -9,8 +9,9 @@ export async function testApiCompatitbility() {
 
     var client = new StructurizrClient(apiKey, apiSecret);
 
+    await wait(1000);
     const resultJson = await client.putWorkspace(parseInt(workspaceId, 10), createWorkspace());
-    const result = JSON.parse(resultJson);
+    const result = tryParse(resultJson);
     expect(result.success).to.be.true;
     expect(result.message).to.equal("OK");
 }
@@ -21,10 +22,28 @@ export async function testApiIdempotency() {
     const apiSecret = process.env.STRUCTURIZR_API_SECRET!
     const workspaceId = process.env.STRUCTURIZR_WORKSPACE_ID!;
     var client = new StructurizrClient(apiKey, apiSecret);
-    const expectedWorkspace =  await client.getWorkspace(parseInt(workspaceId, 10)); 
+    
+    await wait(1000);
+    const expectedWorkspace = await client.getWorkspace(parseInt(workspaceId, 10));
+    
     await testApiCompatitbility();
-    const actualWorkspace = await client.getWorkspace(parseInt(workspaceId, 10)); 
+    
+    await wait(1000);
+    const actualWorkspace = await client.getWorkspace(parseInt(workspaceId, 10));
     expect(actualWorkspace.lastModifiedDate).not.to.be.null;
     actualWorkspace.lastModifiedDate = expectedWorkspace.lastModifiedDate;
-    expect(actualWorkspace.toDto()).to.deep.equalInAnyOrder(expectedWorkspace.toDto());   
+    expect(actualWorkspace.toDto()).to.deep.equalInAnyOrder(expectedWorkspace.toDto());
+}
+
+function tryParse(json: string): any {
+    try {
+        return JSON.parse(json);
+    }
+    catch (e: any) {
+        throw new Error(`Could not parse JSON, error ${e}, json:${json}`);
+    }
+}
+
+function wait(delay: number): Promise<void> {
+    return new Promise(r => setTimeout(r, delay));
 }
